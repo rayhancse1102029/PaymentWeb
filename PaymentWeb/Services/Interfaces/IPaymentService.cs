ab6d1a8201d765f853bef75d266489c8ed46dc31
rayhancse15git <abu@baiust.edu.bd>
2020-05-08 19:15:32 +0600

    sdfdf

---
+++ PaymentWeb/Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PaymentWeb.Data.Entity;
+
+namespace PaymentWeb.Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<bool> SavePaymentDetail(PaymentDetail paymentDetail);
+        Task<IEnumerable<PaymentDetail>> GetAllPaymentDetail();
+        Task<PaymentDetail> GetPaymentDetailById(int id);
+        Task<bool> DeletePaymentDetailById(int id);
+    }
+}
